104b7a8a768e66da3ca2acd8be7f10c37198ba22
fweber144 <fweber144@protonmail.com>
2020-05-26 15:21:16 +0200

    launch_shinybrms.R: Removed argument 'launch.browser' and updated documentation.

--- R/launch_shinybrms.R
+++ R/launch_shinybrms.R
@@ -2,34 +2,35 @@
 #'
 #' @description This function launches the \pkg{shinybrms} Shiny app.
 #'
-#' @param launch.browser Argument `launch.browser` from [shiny::runApp()]. Defaults here to
-#'   `interactive()`.
-#' @param \dots Further arguments passed to [shiny::runApp()]. For example, in order to force
-#'   launching the Shiny app in the system's default web browser, set argument `launch.browser` to
-#'   `TRUE`.
+#' @param \dots Arguments passed to [shiny::runApp()]. For example, in order to force launching the
+#'   Shiny app in the system's default web browser (even if using RStudio), set argument
+#'   `launch.browser` to `TRUE`.
 #'
-#' @note If option `mc.cores` is set, then the advanced option "Cores" defaults to the value of this
-#'   option. If option `mc.cores` is not set, then the advanced option "Cores" defaults to the value
-#'   returned by [parallel::detectCores()].
+#' @note The following global options are introduced by \pkg{shinybrms}:
 #'
-#'   By default, the [Stan](https://mc-stan.org/) progress automatically opens up as an HTML file.
-#'   This behavior may be disabled in the "Advanced options" of the \pkg{shinybrms} Shiny app
-#'   (option "Open progress"), but for the remainder of this paragraph, it is assumed that the
-#'   automatic opening of the Stan progress file is desired. In RStudio, the default for option
-#'   `browser` cannot be used for the Stan progress file. Therefore, \pkg{shinybrms} has an option
-#'   `shinybrms.browser_RStudio` specifying the value for option `browser` to be used when running
-#'   in RStudio. If not specified, option `shinybrms.browser_RStudio` defaults to `NULL`. On
-#'   Windows, `NULL` means that the default browser (as determined by the file association
-#'   mechanism) is used. On non-Windows systems, `NULL` has no meaning and automatically disables
-#'   the advanced option "Open progress" from the \pkg{shinybrms} app internally (with a
-#'   notification popping up) if not done by the user. On any system, option
-#'   `shinybrms.browser_RStudio` may directly specify the HTML browser to use (see argument
-#'   `browser` in [utils::browseURL()] and option `browser` in [base::options()] for details).
+#'   * `shinybrms.prog_browser`: The value for the global option `browser` to be used for opening the
+#'   [Stan](https://mc-stan.org/) progress file (an HTML file). Defaults to option `browser` (see [base::options()]).
+#'   Note that RStudio's default for option `browser` cannot be used for the Stan progress file.
+#'   Therefore, \pkg{shinybrms} automatically redefines RStudio's default for option `browser` to fix
+#'   this issue.
+#'   * `shinybrms.shinystan_browser`: The value for the global option `browser` to be used for opening the
+#'   \pkg{shinystan} app. Defaults to option `browser` (see [base::options()]).
+#'   Note that RStudio's default for option `browser` cannot be used for opening the \pkg{shinystan}
+#'   app from within \pkg{shinybrms}.
+#'   Therefore, \pkg{shinybrms} automatically redefines RStudio's default for option `browser` to fix
+#'   this issue.
 #'
-#'   The option `shinybrms.browser_RStudio` (see above) is also used when launching the
-#'   \pkg{shinystan} Shiny app and running in RStudio. Here, the default (`NULL`) makes it
-#'   impossible to launch the \pkg{shinystan} app if running on a non-Windows system (and in
-#'   RStudio).
+#'   Other global options used by \pkg{shinybrms}:
+#'
+#'   * `mc.cores` (see [base::options()]): If option `mc.cores` is set, then the advanced option
+#'   "Cores" (in the \pkg{shinybrms} app) defaults to the value of this option. If option `mc.cores`
+#'   is not set, then the advanced option "Cores" defaults to the value returned by
+#'   [parallel::detectCores()].
+#'   * `shinystan.rstudio` (see [shinystan::launch_shinystan()]): If set to `TRUE`, then use
+#'   RStudio's mechanism for opening the \pkg{shinystan} app (i.e. use
+#'   `getOption("shiny.launch.browser", interactive())` for argument `launch.browser` of
+#'   [shiny::runApp()]). Defaults to `FALSE` (i.e. use `TRUE` for argument `launch.browser` of
+#'   [shiny::runApp()]).
 #'
 #' @return `TRUE` (invisible).
 #'
@@ -37,17 +38,16 @@
 #'
 #' @examples
 #' if(interactive()){
-#'   launch_shinybrms()
+#'   launch_shinybrms(launch.browser = TRUE)
 #' }
 
-launch_shinybrms <- function(launch.browser = interactive(), ...){
+launch_shinybrms <- function(...){
   app_path <- system.file("shinybrms_app", package = "shinybrms")
   if(identical(app_path, "")){
     stop("Path to package \"shinybrms\" could not be found. Try re-installing package \"shinybrms\".")
   }
   shiny::runApp(
     appDir = app_path,
-    launch.browser = launch.browser,
     ...
   )
   return(invisible(TRUE))

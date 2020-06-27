834790ab29558462b0f5fa1d18363db94d03bc61
fweber144 <fweber144@protonmail.com>
2020-06-27 13:40:25 +0200

    To be safe: Make "n_chains_spec" reactive.

--- inst/shinybrms_app/app.R
+++ inst/shinybrms_app/app.R
@@ -1397,7 +1397,7 @@ server <- function(input, output, session){
   
   # For storing the user-specified number of chains (will be updated when creating C_fit and will be
   # used when printing the warnings):
-  n_chains_spec <- NULL
+  n_chains_spec <- reactiveVal() # NOTE: reactiveVal() is equivalent to reactiveVal(NULL).
   
   C_fit <- eventReactive(input$run_stan, {
     req(C_formula(), C_family(),
@@ -1409,7 +1409,7 @@ server <- function(input, output, session){
         input$advOpts_adapt_delta,
         input$advOpts_max_treedepth)
     
-    n_chains_spec <<- input$advOpts_chains
+    n_chains_spec(input$advOpts_chains)
     
     args_brm <- list(
       formula = C_formula(),
@@ -1539,7 +1539,7 @@ server <- function(input, output, session){
     n_chains_out <- C_fit()$fit@sim$chains
     stopifnot(identical(n_chains_out,
                         dim(as.array(C_fit()$fit))[2]))
-    if(n_chains_out < n_chains_spec){
+    if(n_chains_out < n_chains_spec()){
       fit_warn_tmp <- c(fit_warn_tmp,
                         "Warning: At least one chain exited with an error. The results should not be used.")
     }

289ffe2120acb79c58a84863ecaccd9e04615918
fweber144 <fweber144@protonmail.com>
2020-09-01 16:08:59 +0200

    Revert "Try to avoid skipping unit tests in test-bacteria.R by adopting the code in app.R"
    This reverts commit cf5602570845e8afcd03b2364c16e90aca0f401f.

--- tests/testthat/test-bacteria.R
+++ tests/testthat/test-bacteria.R
@@ -2,6 +2,7 @@ library(shinytest)
 
 test_that("the Stan run for the \"bacteria\" example from the \"shinybrms\" vignette is prepared as expected", {
   skip_on_cran()
+  skip_if_not(identical(Sys.getenv("_R_CHECK_CRAN_INCOMING_"), ""))
   skip_if_not_installed("MASS")
   
   app_path <- system.file("shinybrms_app", package = "shinybrms")

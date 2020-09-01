aba76e3e416e9435519ed8b8a8c2ca1c9ab44e92
fweber144 <fweber144@protonmail.com>
2020-09-01 14:56:26 +0200

    test-bacteria.R: Use 'skip_if_not(interactive())' to ensure that the unit tests are skipped in R CMD CHECK (otherwise, the unit tests fail in R CMD CHECK, even on the same machine as they were created).

--- tests/testthat/test-bacteria.R
+++ tests/testthat/test-bacteria.R
@@ -2,6 +2,7 @@ library(shinytest)
 
 test_that("the Stan run for the \"bacteria\" example from the \"shinybrms\" vignette is prepared as expected", {
   skip_on_cran()
+  skip_if_not(interactive())
   skip_if_not_installed("MASS")
   
   app_path <- system.file("shinybrms_app", package = "shinybrms")

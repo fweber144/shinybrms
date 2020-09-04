0cc4a68c29f98b7a7be040e19ced60e39fd308c7
fweber144 <fweber144@protonmail.com>
2020-09-04 09:25:47 +0200

    test-bacteria.R: Proved that 'compareImages = FALSE' is not necessary in the call 'expect_pass(testApp([...]))'.

--- tests/testthat/test-bacteria.R
+++ tests/testthat/test-bacteria.R
@@ -5,5 +5,5 @@ test_that("the Stan run for the \"bacteria\" example from the \"shinybrms\" vign
   skip_if_not_installed("MASS")
   
   app_path <- system.file("shinybrms_app", package = "shinybrms")
-  expect_pass(testApp(app_path, testnames = "bacteria-prep.R", compareImages = FALSE))
+  expect_pass(testApp(app_path, testnames = "bacteria-prep.R"))
 })

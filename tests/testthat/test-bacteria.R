ca5b657380bbd3ab9b0fe2c4f690891772e3bfd7
fweber144 <fweber144@protonmail.com>
2020-09-04 09:51:42 +0200

    Revert "Tested a different folder structure for "shinytest" (see <https://shiny.rstudio.com/articles/testing-overview.html>)."
    This reverts commit e6beeaab73beeb7fbb9f369f951c2fad40ac72ac.
    
    Reason for revert: This folder structure is not appropriate for a Shiny app bundled in an R package. As may be seen from <https://shiny.rstudio.com/articles/testing-overview.html>, this folder structure is designed for standalone apps.

--- tests/testthat/test-bacteria.R
+++ tests/testthat/test-bacteria.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(shinytest)
+
+test_that("Preparation of the Stan run for the \"bacteria\" example from the \"shinybrms\" vignette", {
+  skip_on_cran()
+  skip_if_not_installed("MASS")
+  
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  expect_pass(testApp(app_path, testnames = "bacteria-prep.R", compareImages = FALSE))
 })

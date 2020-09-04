5687bc4fb550f36fd77a3f5d86051d7d1e6ced6c
fweber144 <fweber144@protonmail.com>
2020-09-04 17:16:08 +0200

    test-bacteria.R: Moved the line 'shinytest::installDependencies()' after the skips (especially after 'skip_on_cran()').

--- tests/testthat/test-bacteria.R
+++ tests/testthat/test-bacteria.R
@@ -1,10 +1,10 @@
 library(shinytest)
-shinytest::installDependencies()
 
 test_that("Preparation of the Stan run for the \"bacteria\" example from the \"shinybrms\" vignette", {
   skip_on_cran()
   skip_if_not_installed("MASS")
   
+  shinytest::installDependencies()
   app_path <- system.file("shinybrms_app", package = "shinybrms")
   expect_pass(testApp(app_path, testnames = "bacteria-prep.R", compareImages = FALSE))
 })

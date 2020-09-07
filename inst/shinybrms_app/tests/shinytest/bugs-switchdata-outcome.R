4563b5b1dd58d807ccdfca55a7dae9244cbe0760
fweber144 <fweber144@protonmail.com>
2020-09-07 17:11:09 +0200

    bugs-switchdata-outcome.R: Simplified the test script.

--- inst/shinybrms_app/tests/shinytest/bugs-switchdata-outcome.R
+++ inst/shinybrms_app/tests/shinytest/bugs-switchdata-outcome.R
@@ -1,11 +1,11 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("bugs-switchdata-outcome", screenshot = FALSE)
 
-app$setInputs(navbar_ID = "Data")
-app$setInputs(ex_da_sel = "Arabidopsis")
-app$setInputs(navbar_ID = "Likelihood")
-app$setInputs(outc_sel = "total.fruits")
-app$setInputs(dist_sel = "negbinomial")
+app$setInputs(navbar_ID = "Data",
+              ex_da_sel = "Arabidopsis")
+app$setInputs(navbar_ID = "Likelihood",
+              outc_sel = "total.fruits",
+              dist_sel = "negbinomial")
 app$setInputs(navbar_ID = "Data")
 app$snapshot()
 app$setInputs(ex_da_sel = "bacteria")

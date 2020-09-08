a9e1ba63f8fbc2b6f920fbeab1bc89b9d0febe66
fweber144 <fweber144@protonmail.com>
2020-09-08 10:41:09 +0200

    Added "bugs_switchData_pred" shinytest.

--- tests/testthat/test-bugs.R
+++ tests/testthat/test-bugs.R
@@ -13,3 +13,17 @@ test_that(
     expect_pass(testApp(app_path, testnames = "bugs_switchData_outcome.R", compareImages = FALSE))
   }
 )
+
+test_that(
+  paste("The app does not crash when switching to a dataset which differs from the older",
+        "dataset only in the name of a predictor variable."),
+  {
+    skip_on_cran()
+    skip_if_not_installed("lme4")
+    skip_if_not_installed("MASS")
+    
+    shinytest::installDependencies()
+    app_path <- system.file("shinybrms_app", package = "shinybrms")
+    expect_pass(testApp(app_path, testnames = "bugs_switchData_pred.R", compareImages = FALSE))
+  }
+)

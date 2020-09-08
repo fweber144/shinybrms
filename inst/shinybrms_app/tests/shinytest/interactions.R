20096b13c6f0c4315fedee03f90d9f9195a1418f
fweber144 <fweber144@protonmail.com>
2020-09-08 13:34:44 +0200

    shinytest "interactions": Simplified the test script.

--- inst/shinybrms_app/tests/shinytest/interactions.R
+++ inst/shinybrms_app/tests/shinytest/interactions.R
@@ -45,7 +45,7 @@ app$setInputs(pred_mainNP_sel = c("rack", "nutrient", "amd", "status", "reg"))
 app$setInputs(pred_int_build = c("reg", "nutrient"),
               pred_int_add = "click")
 app$snapshot()
-app$setInputs(pred_mainNP_sel = c("rack", "nutrient", "amd", "status"))###
+# A test for adding a variable at a specific position (here, "reg" at the beginning):
 app$setInputs(pred_mainNP_sel = c("reg", "rack", "nutrient", "amd", "status"))
 app$setInputs(pred_int_build = c("reg", "nutrient"),
               pred_int_add = "click")

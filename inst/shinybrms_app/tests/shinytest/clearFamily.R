e07115ce459f98291aa8e73ca42852f90699f67e
fweber144 <fweber144@protonmail.com>
2020-09-08 17:19:07 +0200

    clearFamily.R: Simplified the test script.

--- inst/shinybrms_app/tests/shinytest/clearFamily.R
+++ inst/shinybrms_app/tests/shinytest/clearFamily.R
@@ -1,20 +1,17 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("clearFamily", screenshot = FALSE)
 
-app$setInputs(navbar_ID = "Data")
-app$setInputs(ex_da_sel = "bacteria")
-app$setInputs(navbar_ID = "Likelihood")
-app$setInputs(outc_sel = "y")
-app$setInputs(dist_sel = "bernoulli")
-app$setInputs(navbar_ID = "Prior")
-app$setInputs(prior_class_sel = "Intercept")
-app$setInputs(prior_text = "student_t")
-app$setInputs(prior_text = "student_t(3, 0, ")
-app$setInputs(prior_text = "student_t(3, 0, 3")
-app$setInputs(prior_text = "student_t(3, 0, 3)")
-app$setInputs(prior_add = "click")
+app$setInputs(navbar_ID = "Data",
+              ex_da_sel = "bacteria")
+app$setInputs(navbar_ID = "Likelihood",
+              outc_sel = "y",
+              dist_sel = "bernoulli")
+app$setInputs(navbar_ID = "Prior",
+              prior_class_sel = "Intercept",
+              prior_text = "student_t(3, 0, 3)",
+              prior_add = "click")
 app$snapshot()
-app$setInputs(navbar_ID = "Likelihood")
-app$setInputs(dist_sel = "")
+app$setInputs(navbar_ID = "Likelihood",
+              dist_sel = "")
 app$setInputs(navbar_ID = "Prior")
 app$snapshot()

94e8953e9f960e15df9350d7599152a6b27a8dde
fweber144 <fweber144@protonmail.com>
2020-09-08 14:31:15 +0200

    Added "tests/testthat/test-interactions.R".

---
+++ tests/testthat/test-interactions.R
@@ -0,0 +1,10 @@
+library(shinytest)
+
+test_that("Specification of interaction terms.", {
+  skip_on_cran()
+  skip_if_not_installed("lme4")
+  
+  shinytest::installDependencies()
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  expect_pass(testApp(app_path, testnames = "interactions.R", compareImages = FALSE))
+})

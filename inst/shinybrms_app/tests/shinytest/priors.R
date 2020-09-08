4139bfc5f30412ffd94fe028f5dcf34efbc0f729
fweber144 <fweber144@protonmail.com>
2020-09-08 17:53:18 +0200

    priors.R: Simplified the test script.

--- inst/shinybrms_app/tests/shinytest/priors.R
+++ inst/shinybrms_app/tests/shinytest/priors.R
@@ -1,29 +1,24 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("priors", screenshot = FALSE)
 
-app$setInputs(navbar_ID = "Data")
-app$setInputs(ex_da_sel = "bacteria")
-app$setInputs(navbar_ID = "Likelihood")
-app$setInputs(outc_sel = "y")
-app$setInputs(dist_sel = "bernoulli")
+app$setInputs(navbar_ID = "Data",
+              ex_da_sel = "bacteria")
+app$setInputs(navbar_ID = "Likelihood",
+              outc_sel = "y",
+              dist_sel = "bernoulli")
 app$setInputs(likelihood_navlist_ID = "Predictors")
-app$setInputs(pred_mainNP_sel = "week")
-app$setInputs(pred_mainNP_sel = c("week", "trt"))
-app$setInputs(pred_mainPP_sel = "ID")
-app$setInputs(pred_int_build = "week")
-app$setInputs(pred_int_build = c("week", "trt"))
-app$setInputs(pred_int_add = "click")
+app$setInputs(pred_mainNP_sel = c("week", "trt"),
+              pred_mainPP_sel = "ID")
+app$setInputs(pred_int_build = c("week", "trt"),
+              pred_int_add = "click")
 app$setInputs(navbar_ID = "Prior")
 app$snapshot()
 # A test for the bug fixed by commit 4a03135b045b092c91b8d5939bea9965a6b9dc2d:
 app$setInputs(prior_class_sel = "sd")
 app$setInputs(prior_coef_sel = "Intercept")
 app$snapshot()
-app$setInputs(prior_text = "student_")
-app$setInputs(prior_text = "student_t(3, 0, ")
-app$setInputs(prior_text = "student_t(3, 0, 3")
-app$setInputs(prior_text = "student_t(3, 0, 3)")
-app$setInputs(prior_add = "click")
+app$setInputs(prior_text = "student_t(3, 0, 3)",
+              prior_add = "click")
 app$snapshot()
 # A test for the bug fixed by commit 11d32ca228f913f2d55dfbce6735a34e81d89639:
 app$setInputs(prior_coef_sel = "")

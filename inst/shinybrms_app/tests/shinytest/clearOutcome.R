eecc9b013bbb717a548597238a3429c32de1b956
fweber144 <fweber144@protonmail.com>
2020-09-08 17:20:57 +0200

    shinytest "clearOutcome": Removed an unnecessary snapshot.

--- inst/shinybrms_app/tests/shinytest/clearOutcome.R
+++ inst/shinybrms_app/tests/shinytest/clearOutcome.R
@@ -6,9 +6,8 @@ app$setInputs(navbar_ID = "Data",
 app$setInputs(navbar_ID = "Likelihood",
               outc_sel = "y",
               dist_sel = "bernoulli")
-app$setInputs(navbar_ID = "Prior")
-app$snapshot()
-app$setInputs(prior_class_sel = "Intercept",
+app$setInputs(navbar_ID = "Prior",
+              prior_class_sel = "Intercept",
               prior_text = "student_t(3, 0, 3)",
               prior_add = "click")
 app$snapshot()

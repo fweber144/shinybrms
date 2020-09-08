0d26ce8ad026264a4d2afd8f01838a01c0ba306c
fweber144 <fweber144@protonmail.com>
2020-09-08 15:53:53 +0200

    clearData.R: Added a comment concerning the commit that this shinytest refers to.

--- inst/shinybrms_app/tests/shinytest/clearData.R
+++ inst/shinybrms_app/tests/shinytest/clearData.R
@@ -12,6 +12,7 @@ app$setInputs(prior_class_sel = "Intercept",
               prior_text = "student_t(3, 0, 3)",
               prior_add = "click")
 app$snapshot()
+# A test for the bug fixed by commit 32e2ca0b56124ee84ea56bcd21425e22425ad37c:
 app$setInputs(navbar_ID = "Data",
               ex_da_sel = "")
 app$setInputs(navbar_ID = "Prior")

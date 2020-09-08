dd31ffe97d1a4b7f0e2392c8436931a7de167aaf
fweber144 <fweber144@protonmail.com>
2020-09-08 15:48:32 +0200

    clearData.R: Simplified the test script.

--- inst/shinybrms_app/tests/shinytest/clearData.R
+++ inst/shinybrms_app/tests/shinytest/clearData.R
@@ -1,19 +1,19 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("clearData", screenshot = FALSE)
 
-app$setInputs(navbar_ID = "Data")
-app$setInputs(ex_da_sel = "bacteria")
-app$setInputs(navbar_ID = "Likelihood")
-app$setInputs(outc_sel = "y")
-app$setInputs(dist_sel = "bernoulli")
+app$setInputs(navbar_ID = "Data",
+              ex_da_sel = "bacteria")
+app$setInputs(navbar_ID = "Likelihood",
+              outc_sel = "y",
+              dist_sel = "bernoulli")
 app$setInputs(navbar_ID = "Prior")
 app$snapshot()
-app$setInputs(prior_class_sel = "Intercept")
-app$setInputs(prior_text = "student_t(3, 0, 3)")
-app$setInputs(prior_add = "click")
+app$setInputs(prior_class_sel = "Intercept",
+              prior_text = "student_t(3, 0, 3)",
+              prior_add = "click")
 app$snapshot()
-app$setInputs(navbar_ID = "Data")
-app$setInputs(ex_da_sel = "")
+app$setInputs(navbar_ID = "Data",
+              ex_da_sel = "")
 app$setInputs(navbar_ID = "Prior")
 app$snapshot()
 app$setInputs(navbar_ID = "Likelihood")

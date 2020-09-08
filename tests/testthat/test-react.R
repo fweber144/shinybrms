7e5807456f27656d48868aa799d6e9b40cbcd735
fweber144 <fweber144@protonmail.com>
2020-09-08 16:09:12 +0200

    test-react.R: Re-indented the code.

--- tests/testthat/test-react.R
+++ tests/testthat/test-react.R
@@ -26,26 +26,20 @@ test_that(
   }
 )
 
-test_that(
-  paste("clearing the outcome resets the priors."),
-  {
-    skip_on_cran()
-    skip_if_not_installed("MASS")
-    
-    shinytest::installDependencies()
-    app_path <- system.file("shinybrms_app", package = "shinybrms")
-    expect_pass(testApp(app_path, testnames = "clearOutcome.R", compareImages = FALSE))
-  }
-)
+test_that("clearing the outcome resets the priors.", {
+  skip_on_cran()
+  skip_if_not_installed("MASS")
+  
+  shinytest::installDependencies()
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  expect_pass(testApp(app_path, testnames = "clearOutcome.R", compareImages = FALSE))
+})
 
-test_that(
-  paste("clearing the example dataset resets the priors and the selected outcome."),
-  {
-    skip_on_cran()
-    skip_if_not_installed("MASS")
-    
-    shinytest::installDependencies()
-    app_path <- system.file("shinybrms_app", package = "shinybrms")
-    expect_pass(testApp(app_path, testnames = "clearData.R", compareImages = FALSE))
-  }
-)
+test_that("clearing the example dataset resets the priors and the selected outcome.", {
+  skip_on_cran()
+  skip_if_not_installed("MASS")
+  
+  shinytest::installDependencies()
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  expect_pass(testApp(app_path, testnames = "clearData.R", compareImages = FALSE))
+})

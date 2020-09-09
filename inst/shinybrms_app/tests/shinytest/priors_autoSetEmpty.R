26bfa22a7a5fa7708fb23a26173d42c99231af5a
fweber144 <fweber144@protonmail.com>
2020-09-09 12:20:34 +0200

    priors_autoSetEmpty.R: Simplified the test script.

--- inst/shinybrms_app/tests/shinytest/priors_autoSetEmpty.R
+++ inst/shinybrms_app/tests/shinytest/priors_autoSetEmpty.R
@@ -1,19 +1,18 @@
 app <- ShinyDriver$new("../../")
 app$snapshotInit("priors_autoSetEmpty", screenshot = FALSE)
 
-app$setInputs(navbar_ID = "Data")
-app$setInputs(ex_da_sel = "bacteria")
-app$setInputs(navbar_ID = "Likelihood")
-app$setInputs(outc_sel = "y")
-app$setInputs(dist_sel = "bernoulli")
-app$setInputs(likelihood_navlist_ID = "Predictors")
-app$setInputs(pred_mainNP_sel = "week")
-app$setInputs(pred_mainPP_sel = "ID")
-app$setInputs(pred_int_build = "week")
-app$setInputs(pred_int_build = c("week", "ID"))
-app$setInputs(pred_int_add = "click")
-app$setInputs(navbar_ID = "Prior")
-app$setInputs(prior_class_sel = "sd")
+app$setInputs(navbar_ID = "Data",
+              ex_da_sel = "bacteria")
+app$setInputs(navbar_ID = "Likelihood",
+              outc_sel = "y",
+              dist_sel = "bernoulli")
+app$setInputs(likelihood_navlist_ID = "Predictors",
+              pred_mainNP_sel = "week",
+              pred_mainPP_sel = "ID")
+app$setInputs(pred_int_build = c("week", "ID"),
+              pred_int_add = "click")
+app$setInputs(navbar_ID = "Prior",
+              prior_class_sel = "sd")
 app$setInputs(prior_group_sel = "ID")
 app$snapshot()
 app$setInputs(prior_class_sel = "cor")

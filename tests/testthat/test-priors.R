d2f5f0a46c908823ba672de72fac763c22579d09
fweber144 <fweber144@protonmail.com>
2020-09-09 11:57:05 +0200

    Added shinytest "priors_autoSetEmpty".

--- tests/testthat/test-priors.R
+++ tests/testthat/test-priors.R
@@ -8,3 +8,12 @@ test_that("Specification of custom priors", {
   app_path <- system.file("shinybrms_app", package = "shinybrms")
   expect_pass(testApp(app_path, testnames = "priors.R", compareImages = FALSE))
 })
+
+test_that("the group for the prior is not automatically set to be empty when not necessary.", {
+  skip_on_cran()
+  skip_if_not_installed("MASS")
+  
+  shinytest::installDependencies()
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  expect_pass(testApp(app_path, testnames = "priors_autoSetEmpty.R", compareImages = FALSE))
+})

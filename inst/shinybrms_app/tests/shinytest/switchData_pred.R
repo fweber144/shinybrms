3902ceb8f1759d21d8d3457410b73cf88fbb3df5
fweber144 <fweber144@protonmail.com>
2020-09-10 10:18:37 +0200

    shinytest "switchData_pred": Use a different (more realistic; simulated) dataset.

--- inst/shinybrms_app/tests/shinytest/switchData_pred.R
+++ inst/shinybrms_app/tests/shinytest/switchData_pred.R
@@ -2,18 +2,18 @@ app <- ShinyDriver$new("../../")
 app$snapshotInit("switchData_pred", screenshot = FALSE)
 
 app$setInputs(navbar_ID = "Data")
-app$uploadFile(file_upload = "switchData_pred-data.csv")
+app$uploadFile(file_upload = "switchData-data.csv")
 app$setInputs(navbar_ID = "Likelihood",
-              outc_sel = "column1",
+              outc_sel = "y",
               dist_sel = "bernoulli")
 app$setInputs(likelihood_navlist_ID = "Predictors",
-              pred_mainNP_sel = "column2")
+              pred_mainNP_sel = "x1")
 app$setInputs(navbar_ID = "Data")
 app$snapshot(items = list(input = setdiff(app$listWidgets()$input, "file_upload"),
                           output = TRUE,
                           export = TRUE))
 # A test for the bug fixed by commit 490a653dce15fd7351586097b6724e9aa6d71f4b:
-app$uploadFile(file_upload = "switchData_pred-data_mod.csv")
+app$uploadFile(file_upload = "switchData_pred-data.csv")
 app$snapshot(items = list(input = setdiff(app$listWidgets()$input, "file_upload"),
                           output = TRUE,
                           export = TRUE))

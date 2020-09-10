a007eec72c2fe251dfe02de904d53c92ba786ff7
fweber144 <fweber144@protonmail.com>
2020-09-10 10:46:35 +0200

    Added shinytest "switchData_value".

--- tests/testthat/test-react.R
+++ tests/testthat/test-react.R
@@ -34,6 +34,18 @@ test_that("uploading another dataset (here: the same dataset again) resets the c
   expect_pass(testApp(app_path, testnames = "switchData_same.R", compareImages = FALSE))
 })
 
+test_that(
+  paste("switching to a dataset which differs from the former dataset only in some values of the variables",
+        "causes the default priors to be recomputed correctly and the custom priors to be reset."),
+  {
+    skip_on_cran()
+    
+    shinytest::installDependencies()
+    app_path <- system.file("shinybrms_app", package = "shinybrms")
+    expect_pass(testApp(app_path, testnames = "switchData_value.R", compareImages = FALSE))
+  }
+)
+
 test_that("clearing the outcome resets the priors.", {
   skip_on_cran()
   skip_if_not_installed("MASS")

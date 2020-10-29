8c886e48c18ecadf5660d122d4d6a74b4fa22203
fweber144 <fweber144@protonmail.com>
2020-10-29 16:10:02 +0100

    Tab "Conditional effects": Removed an unnecessary comment.

--- inst/shinybrms_app/app.R
+++ inst/shinybrms_app/app.R
@@ -965,7 +965,6 @@ ui <- navbarPage(
         helpText(
           "The \"conditional effects\" plot shows how the outcome behaves as a function of",
           "a predictor variable or as a function of an interaction.",
-          # "For interactions, only interaction effects involving at most 2 predictor variables may be plotted."
           "Note that interaction effects of order higher than 2 cannot be plotted here, so only main effects",
           "as well as interaction effects involving at most 2 predictor variables may be plotted."
         ),

13dd0d532bd0334bffa3b9b4a891d5808fa884ef
fweber144 <fweber144@protonmail.com>
2021-03-06 15:22:24 +0100

    Update the copyright date.

--- inst/shinybrms_app/app.R
+++ inst/shinybrms_app/app.R
@@ -1,5 +1,5 @@
 # shinybrms: Graphical User Interface ('shiny' App) for 'brms'
-# Copyright (C) 2020  Frank Weber
+# Copyright (C) 2021  Frank Weber
 #   
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

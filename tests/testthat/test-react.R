c8a2ebf9e4c6329360c0178cb432fe4373183e23
fweber144 <fweber144@protonmail.com>
2021-10-09 21:18:25 +0200

    Improve indentation of long test names.

--- tests/testthat/test-react.R
+++ tests/testthat/test-react.R
@@ -1,30 +1,28 @@
 library(shinytest)
 
-test_that(
-  paste("the app does not crash when switching the dataset after having selected",
-        "outcome and distributional family (but no predictors) for the initial dataset."),
-  {
-    skip_on_cran()
-    skip_if_not_installed("lme4")
-    skip_if_not_installed("MASS")
-    
-    shinytest::installDependencies()
-    app_path <- system.file("shinybrms_app", package = "shinybrms")
-    expect_pass(testApp(app_path, testnames = "switchData_outcome.R"))
-  }
-)
+test_that(paste(
+  "the app does not crash when switching the dataset after having selected",
+  "outcome and distributional family (but no predictors) for the initial dataset."
+), {
+  skip_on_cran()
+  skip_if_not_installed("lme4")
+  skip_if_not_installed("MASS")
+  
+  shinytest::installDependencies()
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  expect_pass(testApp(app_path, testnames = "switchData_outcome.R"))
+})
 
-test_that(
-  paste("the app does not crash when switching to a dataset which differs from",
-        "the initial dataset only in the name of a predictor variable."),
-  {
-    skip_on_cran()
-    
-    shinytest::installDependencies()
-    app_path <- system.file("shinybrms_app", package = "shinybrms")
-    expect_pass(testApp(app_path, testnames = "switchData_pred.R"))
-  }
-)
+test_that(paste(
+  "the app does not crash when switching to a dataset which differs from",
+  "the initial dataset only in the name of a predictor variable."
+), {
+  skip_on_cran()
+  
+  shinytest::installDependencies()
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  expect_pass(testApp(app_path, testnames = "switchData_pred.R"))
+})
 
 test_that("uploading another dataset (here: the same dataset again) resets the custom priors.", {
   skip_on_cran()
@@ -34,17 +32,16 @@ test_that("uploading another dataset (here: the same dataset again) resets the c
   expect_pass(testApp(app_path, testnames = "switchData_same.R"))
 })
 
-test_that(
-  paste("switching to a dataset which differs from the initial dataset only in some values of the variables",
-        "causes the default priors to be recomputed correctly and the custom priors to be reset."),
-  {
-    skip_on_cran()
-    
-    shinytest::installDependencies()
-    app_path <- system.file("shinybrms_app", package = "shinybrms")
-    expect_pass(testApp(app_path, testnames = "switchData_value.R"))
-  }
-)
+test_that(paste(
+  "switching to a dataset which differs from the initial dataset only in some values of the variables",
+  "causes the default priors to be recomputed correctly and the custom priors to be reset."
+), {
+  skip_on_cran()
+  
+  shinytest::installDependencies()
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  expect_pass(testApp(app_path, testnames = "switchData_value.R"))
+})
 
 test_that("clearing the outcome resets the priors.", {
   skip_on_cran()

b66fe12100cfa5776f2d2c3e5b5e3d2bb7a5dfd2
fweber144 <fweber144@protonmail.com>
2021-10-14 09:21:39 +0200

    Revert to the old theme (customized "United" Bootstrap 3 theme).

--- inst/shinybrms_app/app.R
+++ inst/shinybrms_app/app.R
@@ -1262,14 +1262,7 @@ ui <- navbarPage(
     )
   ),
   tabPanel(title = "Quit", value = "quit_app", icon = icon("power-off")),
-  theme = bslib::bs_theme(
-    version = 4,
-    bootswatch = "united",
-    primary = "#a50034",
-    secondary = "#858182",
-    base_font = "Verdana",
-    font_scale = 0.9
-  )
+  theme = "united_mod.min.css"
 )
 
 ####################################################################################################

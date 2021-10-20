ce0dcd7796d9f73e428668d5a5544103ac19a9d4
fweber144 <fweber144@protonmail.com>
2021-10-20 15:04:36 +0200

    Revert "Demonstrate usage of argument `suffix` in `shinytest::testApp()`."
    This reverts commit 4870f4b40a28aad74784e0f145fe13bc14fd1a0b.

--- tests/testthat/test-bacteria.R
+++ tests/testthat/test-bacteria.R
@@ -1,6 +1,6 @@
 library(shinytest)
 
-test_that("Results of the Stan run for the \"bacteria\" example from the \"shinybrms\" vignette", {
+test_that("Results of the Stan run for the \"bacteria\" example from the \"shinybrms\" vignette on Linux", {
   skip_on_cran()
   skip_on_ci()
   skip_on_covr()
@@ -9,13 +9,17 @@ test_that("Results of the Stan run for the \"bacteria\" example from the \"shiny
   
   shinytest::installDependencies()
   app_path <- system.file("shinybrms_app", package = "shinybrms")
-  # Suffix for the OS (one could simply use shinytest::osName() instead, but
-  # that would require changing the names of the folders for the expected test
-  # results):
-  if (identical(Sys.info()["sysname"], setNames("Linux", "sysname"))) {
-    os_suffix <- "linux"
-  } else {
-    os_suffix <- .Platform$OS.type
-  }
-  shinytest::expect_pass(shinytest::testApp(app_path, testnames = "bacteria_run.R", suffix = os_suffix))
+  shinytest::expect_pass(shinytest::testApp(app_path, testnames = "bacteria_run_linux.R"))
+})
+
+test_that("Results of the Stan run for the \"bacteria\" example from the \"shinybrms\" vignette on Windows", {
+  skip_on_cran()
+  skip_on_ci()
+  skip_on_covr()
+  skip_if_not_installed("MASS")
+  skip_if_not(identical(.Platform$OS.type, "windows"))
+  
+  shinytest::installDependencies()
+  app_path <- system.file("shinybrms_app", package = "shinybrms")
+  shinytest::expect_pass(shinytest::testApp(app_path, testnames = "bacteria_run_windows.R"))
 })

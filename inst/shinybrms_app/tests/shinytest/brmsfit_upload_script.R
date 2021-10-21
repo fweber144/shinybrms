903f3c1cc97b08cc46425b58a94bc474859a5f59
fweber144 <fweber144@protonmail.com>
2021-10-21 22:24:32 +0200

    Ensure that the `brmsfit_upload` shinytest also runs in `R CMD check`.

--- inst/shinybrms_app/tests/shinytest/brmsfit_upload_script.R
+++ inst/shinybrms_app/tests/shinytest/brmsfit_upload_script.R
@@ -10,8 +10,7 @@ if (identical(Sys.info()["sysname"], setNames("Linux", "sysname"))) {
 app$setInputs(advOpts_cores = 2, wait_ = FALSE, values_ = FALSE)
 
 app$setInputs(navbar_ID = "Posterior")
-app$uploadFile(brmsfit_upload = file.path(paste0("bacteria_run_", os_chr, "-expected"),
-                                          "bacteria_full.rds"))
+app$uploadFile(brmsfit_upload = file.path("bacteria_full.rds"))
 app$setInputs(posterior_navlist_ID = "MCMC diagnostics",
               show_general_MCMC_tab = TRUE)
 app$setInputs(posterior_navlist_ID = "Default summary")
@@ -30,7 +29,17 @@ app$setInputs(cust_name = "week for drugP",
 app$setInputs(posterior_navlist_ID = "Conditional effects",
               term_sel = "week:trt")
 app$setInputs(posterior_navlist_ID = "Launch <strong>shinystan</strong>")
-app$snapshot(items = list(input = TRUE,
+### Needed because `brmsfit_upload`'s element `"size"` is highly sensitive to
+### even small changes (like the date stored in the `brmsfit`, etc.); note that
+### app$getValue() does not work here:
+bfit_upld <- app$getAllValues(input = "brmsfit_upload",
+                              output = FALSE,
+                              export = FALSE)$input$brmsfit_upload
+bfit_upld <- bfit_upld[, setdiff(names(bfit_upld), "size"), drop = FALSE]
+stopifnot(identical(
+  bfit_upld, data.frame(name = "bacteria_full.rds", type = "", datapath = "0.rds")
+))
+app$snapshot(items = list(input = setdiff(app$listWidgets()$input, "brmsfit_upload"),###
                           output = setdiff(app$listWidgets()$output, "fit_date"),
                           export = TRUE),
              filename = "post_full.json")

6ae19d8bc9b24f489d2144975f57185296c256de
fweber144 <fweber144@protonmail.com>
2022-04-02 21:00:44 +0200

    brms issue 1257 is fixed in brms versions > 2.16.3

--- inst/shinybrms_app/app.R
+++ inst/shinybrms_app/app.R
@@ -2396,7 +2396,7 @@ server <- function(input, output, session) {
         )
         req(FALSE)
       }
-      if (save_warmup_tmp) {
+      if (save_warmup_tmp && packageVersion("brms") <= "2.16.3") {
         showNotification(
           HTML(paste0(
             "Because of ", strong("brms", .noWS = "outside"), "'s issue #1257 ",

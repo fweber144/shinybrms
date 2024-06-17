f4f3a0e1dfb1c46e976758c1b565d444e8ac3127
fweber144 <fweber144@protonmail.com>
2024-06-17 21:50:45 +0200

    Avoid that `tests/testthat/test-shinybrms.R` contains an "empy" test which will be marked as skipped.

--- tests/testthat/test-shinybrms.R
+++ tests/testthat/test-shinybrms.R
@@ -3,4 +3,5 @@ library(shinytest2)
 test_that("shinybrms passes all shinytest2 tests", {
   skip_on_cran()
   shinytest2::test_app(system.file("shinybrms_app", package = "shinybrms"))
+  expect_true(TRUE)
 })

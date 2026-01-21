8a06182f9a92c2630b5abbe2d6a05841184f99f4
fweber144 <fweber144@protonmail.com>
2026-01-21 21:12:45 +0100

    remove outdated test files and start to modify the remaining test files (here: `test-shinytest2.R`)

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,7 +1,11 @@
 library(shinytest2)
 
-test_that("Initial Shiny values are consistent", {
-  app <- AppDriver$new(expect_values_screenshot_args = FALSE)
+test_that("initial Shiny values are consistent", {
+  skip_on_cran()
+  appdir <- system.file(package = "shinybrms", "shinybrms_app")
+  local_app_support(appdir)
+  app <- AppDriver$new(appdir, name = "init",
+                       expect_values_screenshot_args = FALSE)
   
   app$expect_values()
   
